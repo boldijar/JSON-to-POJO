9d80fad896da33b1e3bfa54ee7e77554aaf6b52d
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-24 16:40:58 +0300

    removed unused imports

--- src/main/java/com/boldijarpaul/jsontopojo/helper/JsonHelper.java
+++ src/main/java/com/boldijarpaul/jsontopojo/helper/JsonHelper.java
@@ -3,13 +3,11 @@ package com.boldijarpaul.jsontopojo.helper;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.google.gson.JsonObject;
-
 public class JsonHelper {
 	public static boolean jsonValid(String json) {
 
 		try {
-			JSONObject jsonObject = new JSONObject(json);
+			new JSONObject(json);
 			return true;
 		} catch (JSONException e) {
 			return false;
--- src/main/java/com/boldijarpaul/jsontopojo/servlets/HomeServlet.java
+++ src/main/java/com/boldijarpaul/jsontopojo/servlets/HomeServlet.java
@@ -2,7 +2,6 @@ package com.boldijarpaul.jsontopojo.servlets;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/")
