490f34529ebea6d4945b4979b44b081835f07a59
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-24 12:15:47 +0300

    added Variable.java

---
+++ src/main/java/com/boldijarpaul/jsontopojo/entities/Variable.java
@@ -0,0 +1,24 @@
+package com.boldijarpaul.jsontopojo.entities;
+
+public class Variable {
+
+	private String name;
+	private ObjectType type;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ObjectType getType() {
+		return type;
+	}
+
+	public void setType(ObjectType type) {
+		this.type = type;
+	}
+
+}
