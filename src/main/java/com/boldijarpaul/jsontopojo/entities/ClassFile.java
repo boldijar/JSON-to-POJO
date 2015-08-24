b5f85119947f040d1d3727f6235783bbcb315155
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-24 13:42:32 +0300

    added class file

---
+++ src/main/java/com/boldijarpaul/jsontopojo/entities/ClassFile.java
@@ -0,0 +1,19 @@
+package com.boldijarpaul.jsontopojo.entities;
+
+public class ClassFile {
+	private String text;
+	private String filename;
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	
+}
