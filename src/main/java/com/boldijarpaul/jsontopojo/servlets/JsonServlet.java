d73566a6dcc835a39ad1c8d50ee8931a20dfbb93
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-24 13:04:39 +0300

    added post method

--- src/main/java/com/boldijarpaul/jsontopojo/servlets/JsonServlet.java
+++ src/main/java/com/boldijarpaul/jsontopojo/servlets/JsonServlet.java
@@ -2,13 +2,28 @@ package com.boldijarpaul.jsontopojo.servlets;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.boldijarpaul.jsontopojo.entities.ClassObject;
+import com.boldijarpaul.jsontopojo.helper.JsonHelper;
+import com.boldijarpaul.jsontopojo.parser.ClassObjectConverter;
+import com.boldijarpaul.jsontopojo.parser.ParseJson;
 
 @Path("/")
 public class JsonServlet {
-	
+
 	@POST
-	@Path("/json")
-	public String getImage() {
-		return "Give me a star! https://github.com/BoldijarPaul/JSON-to-POJO";
+	@Path("/json/{root}")
+	public Response getClasses(String json, @PathParam("root") String root) {
+		if (!JsonHelper.jsonValid(json)) {
+			/* invalid json */
+			return Response.status(Status.BAD_REQUEST).entity("JSON error")
+					.build();
+		}
+		ClassObject classObject = ParseJson.parse(json, root);
+		//ClassObject[] classObjects=ClassObjectConverter.getAllClassObjects(classObject, classObjects)
+
 	}
 }
