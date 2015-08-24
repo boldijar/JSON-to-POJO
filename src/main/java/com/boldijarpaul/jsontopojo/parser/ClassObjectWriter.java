e45b2d4e59b93b45f6f70e8a3ae3771ece077902
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-24 12:38:59 +0300

    added class to export the class objects

---
+++ src/main/java/com/boldijarpaul/jsontopojo/parser/ClassObjectWriter.java
@@ -0,0 +1,17 @@
+package com.boldijarpaul.jsontopojo.parser;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import com.boldijarpaul.jsontopojo.entities.ClassObject;
+
+public class ClassObjectWriter {
+
+	/* will write the class file */
+	public static void writeClassObject(ClassObject classObject) throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter(classObject.getName(), "UTF-8");
+		writer.print(ClassObjectConverter.toPojo(classObject));
+		writer.close();
+	}
+}
