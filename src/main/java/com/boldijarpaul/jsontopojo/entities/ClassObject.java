ef3989230f54b7b39d96e08c631faa0c3fe098e1
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-24 12:16:01 +0300

    added ClassObject.java

---
+++ src/main/java/com/boldijarpaul/jsontopojo/entities/ClassObject.java
@@ -0,0 +1,47 @@
+package com.boldijarpaul.jsontopojo.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassObject {
+
+	private String name;
+	private String json;
+
+	private List<Variable> primiveVariables = new ArrayList<Variable>();
+	private List<ClassObject> objects = new ArrayList<ClassObject>();
+
+	public List<Variable> getPrimiveVariables() {
+		return primiveVariables;
+	}
+
+	public void setPrimiveVariables(List<Variable> primiveVariables) {
+		this.primiveVariables = primiveVariables;
+	}
+
+	public List<ClassObject> getObjects() {
+		return objects;
+	}
+
+	public void setObjects(List<ClassObject> objects) {
+		this.objects = objects;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJson() {
+		return json;
+	}
+
+	public void setJson(String json) {
+		this.json = json;
+	}
+	
+
+}
