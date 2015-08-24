166af3d2c60c297f54903fd7031924b278caab1a
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-24 12:11:40 +0300

    initial commit

---
+++ src/main/java/com/boldijarpaul/jsontopojo/servlets/HomeServlet.java
@@ -0,0 +1,18 @@
+package com.boldijarpaul.jsontopojo.servlets;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+ 
+
+@Path("/")
+public class HomeServlet {
+
+	@GET
+	@Path("")
+	public String getImage() {
+		return "Give me a star! https://github.com/BoldijarPaul/JSON-to-POJO";
+	}
+
+}
