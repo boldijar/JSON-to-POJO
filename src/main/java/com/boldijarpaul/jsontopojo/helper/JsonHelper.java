c2ab78330b93e80eb552a6de0b580df5da38848d
Boldijar Paul <paul.bv96@yahoo.com>
2015-08-28 01:32:29 +0300

    validating json for array as well

--- src/main/java/com/boldijarpaul/jsontopojo/helper/JsonHelper.java
+++ src/main/java/com/boldijarpaul/jsontopojo/helper/JsonHelper.java
@@ -1,16 +1,25 @@
 package com.boldijarpaul.jsontopojo.helper;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonHelper {
+	/* returns true if json is json object or json array */
 	public static boolean jsonValid(String json) {
 
 		try {
 			new JSONObject(json);
 			return true;
 		} catch (JSONException e) {
-			return false;
+
+			try {
+				new JSONArray(json);
+				return true;
+			} catch (JSONException e2) {
+				return false;
+			}
+
 		}
 
 	}
